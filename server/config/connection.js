372df3e3132c8f26b459779e06e5462b3d383d02
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-04-26 16:59:50 +1000

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:0/googlebooks_DB');
 
 module.exports = mongoose.connection;
